65faaf0be0ff79d9330225cc289ef6b35031438c
Maciek <maslo12@gmail.com>
2020-11-24 17:01:43 +0100

    I

---
+++ tests/Feature/PostsTest.php
@@ -0,0 +1,26 @@
+<?php
+// tests/Feature/PostsTest.php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use Tests\TestCase;
+
+class PostsTest extends TestCase
+{
+	/** @test */
+    public function the_posts_show_route_can_be_accessed()
+    {
+        // Arrange
+        // Dodajemy do bazy danych wpis
+    		$post = Post::create([
+            'title' => 'Wrabiał krowę w morderstwo cioci',]);
+        // Act
+        // Wykonujemy zapytanie pod adres wpisu
+            $response = $this->get('/posts/' . $post->id);
+        // Assert
+        // Sprawdzamy że w odpowiedzi znajduje się tytuł wpisu
+            $response->assertStatus(200)
+            ->assertSeeText('Wrabiał krowę w morderstwo cioci');
+    }
+}
